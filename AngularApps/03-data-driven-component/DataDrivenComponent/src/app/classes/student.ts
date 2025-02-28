388ef6d1098c2e42b6110abbd07d64363be5c756
Martin Molema <Martin.Molema@nhlstenden.com>
2025-02-28 16:05:30 +0100

    renamed folder

---
+++ AngularApps/03-data-driven-component/DataDrivenComponent/src/app/classes/student.ts
@@ -0,0 +1,14 @@
+export class Student {
+  firstName: string;
+  email: string;
+  phone: string;
+  lastName: string;
+
+  constructor(firstName: string, email: string, phone: string, lastName: string) {
+    this.firstName = firstName;
+    this.email = email;
+    this.phone = phone;
+    this.lastName = lastName;
+  }
+}
+
