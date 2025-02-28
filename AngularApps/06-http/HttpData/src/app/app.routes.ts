8b7f05ab505d935243094e588f32e0090bb47e3f
Martin Molema <Martin.Molema@nhlstenden.com>
2025-02-28 16:06:12 +0100

    renamed folder

--- AngularApps/06-http/HttpData/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import {StallingListComponent} from './components/stalling-list/stalling-list.component';
-import {StallingInfoComponent} from './components/stalling-info/stalling-info.component';
-
-export const routes: Routes = [
-  { path: '', component: StallingListComponent },
-  { path: 'stalling/:id', component: StallingInfoComponent },
-];
--- AngularApps/06-http/HttpData/src/app/classes/stalling-data.spec.ts
+++
@@ -1,7 +0,0 @@
-import { StallingData } from './stalling-data';
-
-describe('StallingData', () => {
-  it('should create an instance', () => {
-    expect(new StallingData()).toBeTruthy();
-  });
-});
