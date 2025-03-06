4531c065ffd160688554477a8e1139b239b5bb8a
Martin Molema <Martin.Molema@nhlstenden.com>
2025-03-06 14:10:50 +0100

    Fix wrong parameter order

--- AngularApps/03-data-driven-component/DataDrivenComponent/src/app/classes/student.ts
+++ AngularApps/03-data-driven-component/DataDrivenComponent/src/app/classes/student.ts
@@ -1,14 +1,14 @@
 export class Student {
   firstName: string;
-  email: string;
-  phone: string;
   lastName: string;
+  phone: string;
+  email: string;
 
-  constructor(firstName: string, email: string, phone: string, lastName: string) {
+  constructor(firstName: string, lastName: string, phone: string, email: string) {
     this.firstName = firstName;
-    this.email = email;
-    this.phone = phone;
     this.lastName = lastName;
+    this.phone = phone;
+    this.email = email;
   }
 }
 
