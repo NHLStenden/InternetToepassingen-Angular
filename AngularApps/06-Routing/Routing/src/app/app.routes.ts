1faa075474849278cb7804f8092f650c9f98522b
Martin Molema <Martin.Molema@nhlstenden.com>
2026-03-09 12:46:31 +0100

    Added a dashboard from which to navigate

--- AngularApps/06-Routing/Routing/src/app/app.routes.ts
+++ AngularApps/06-Routing/Routing/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {StudentListComponent} from './components/student-list/student-list.component';
 import {StudentInfoComponent} from './components/student-info/student-info.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
-  { path: '', component: StudentListComponent },
-  { path: 'student/:id', component: StudentInfoComponent },
+  { path: '', component: DashboardComponent },
+  { path: 'students', component: StudentListComponent },
+  { path: 'students/:id', component: StudentInfoComponent },
 ];
